caa70bcf497d12b9e0368f4a1af96c1dab8c2f97
Jéssica Marinho <jlmarinhocosta@gmail.com>
2021-07-22 15:50:09 -0300

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,117 @@
+variable "artists" {
+    type = list(map(string))
+    default = [
+        {
+            "singer" = "Aerosmith"
+            "song"   = "Dream On"
+        },
+        {
+            "singer" = "Jimi Hendrix"
+            "song"   = "All Along the Watchtower"
+        },
+        {
+            "singer" = "The Killers"
+            "song"   = "Glamorous Indie Rock And Roll"
+        },
+        {
+            "singer" = "The Strokes"
+            "song"   = "Taken for a Fool"
+        },
+        {
+            "singer" = "Arctic Monkeys"
+            "song"   = "I Bet You Look Good On the Dancefloor"
+        },
+        {
+            "singer" = "The Neighbourhood"
+            "song"   = "Daddy Issues"
+        },
+        {
+            "singer" = "The Neighbourhood"
+            "song"   = "Female Robbery"
+        },
+        {
+            "singer" = "Mando Diao"
+            "song"   = "Dance With Somebody"
+        },
+        {
+            "singer" = "The Subways"
+            "song"   = "Rock & Roll Queen"
+        },
+        {
+            "singer" = "The Subways"
+            "song"   = "Oh Yeah"
+        },
+        {
+            "singer" = "The Bravery"
+            "song"   = "An Honest Mistake"
+        },
+        {
+            "singer" = "The Last Shadow Puppets"
+            "song"   = "Aviation"
+        },
+        {
+            "singer" = "The Kooks"
+            "song"   = "Bad Habit"
+        },
+        {
+            "singer" = "South"
+            "song"   = "Paint The Silence"
+        },
+        {
+            "singer" = "New Order"
+            "song"   = "Crystal"
+        },
+        {
+            "singer" = "David Bowie"
+            "song"   = "Ashes to Ashes"
+        },
+        {
+            "singer" = "The White Stripes"
+            "song"   = "Icky Thump"
+        },
+        {
+            "singer" = "Wolfmother"
+            "song"   = "New Moon Rising"
+        },
+        {
+            "singer" = "Foo Fighters"
+            "song"   = "Arlandria"
+        },
+        {
+            "singer" = "The Smashing Pumpkins"
+            "song"   = "Ava Adore"
+        },
+        {
+            "singer" = "Bush"
+            "song"   = "Machinehead"
+        },
+        {
+            "singer" = "The Rapture"
+            "song"   = "How Deep Is Your Love?"
+        },
+        {
+            "singer" = "Arcade Fire"
+            "song"   = "Reflektor"
+        },
+        {
+            "singer" = "Bring Me The Horizon"
+            "song"   = "Throne"
+        },
+        {
+            "singer" = "Rise Against"
+            "song"   = "Satellite"
+        },
+        {
+            "singer" = "Anberlin"
+            "song"   = "Armageddon"
+        },
+        {
+            "singer" = "The Smashing Pumpkins"
+            "song"   = "Eye"
+        },
+        {
+            "singer" = "Tame Impala"
+            "song"   = "Borderline"
+        },
+    ]
+}
